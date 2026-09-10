e2414720eba1bc5c63f4469c7fbdb97d42ce831f
Guilherme Machado <guilherme.bianchi.machado@gmail.com>
2026-09-10 12:08:30 -0300

    fix: add try-catch to validate data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import exception.BibliotecaException;
 import model.Livro;
 import model.Usuario;
 import repository.EmprestimoRepository;
@@ -5,6 +6,7 @@ import repository.LivroRepository;
 import repository.UsuarioRepository;
 import service.EmprestimoService;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +17,7 @@ public class Main {
         EmprestimoService emprestimoService = new EmprestimoService(livroRepository, usuarioRepository, emprestimoRepository);
         Scanner sc = new Scanner(System.in);
 
+        mainLoop:
         while (true) {
             exibirMenu();
             int op = sc.nextInt();
@@ -27,10 +30,24 @@ public class Main {
                     cadastrarUsuario(sc, usuarioRepository);
                     break;
                 case 3:
+                    emprestarLivro(sc, emprestimoService);
+                    break;
 
             }
         }
 
+
+
+    }
+
+    private int lerInteiro(String message, Scanner sc) {
+        try {
+            System.out.println(message);
+            return sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Digite um número válido");
+            sc.nextLine();
+        }
     }
 
     public static void exibirMenu() {
@@ -62,14 +79,22 @@ public class Main {
     }
 
     public static void cadastrarUsuario(Scanner sc, UsuarioRepository usuarioRepository) {
-        System.out.print("\nId: ");
-        int id = sc.nextInt();
-        System.out.print("\nNome: ");
-        String nome = sc.next();
-        System.out.print("\nSenha: ");
-        String senha = sc.next();
-        System.out.print("\nEmail: ");
-        String email = sc.next();
+        int id = 0;
+        String nome = null;
+        String senha = null;
+        String email = null;
+        try {
+            System.out.print("\nId: ");
+            id = sc.nextInt();
+            System.out.print("\nNome: ");
+            nome = sc.next();
+            System.out.print("\nSenha: ");
+            senha = sc.next();
+            System.out.print("\nEmail: ");
+            email = sc.next();
+        } catch (InputMismatchException e) {
+            throw new RuntimeException("Insira um valor válido");
+        }
 
         Usuario usuario = new Usuario(id, nome, senha, email);
         usuarioRepository.adicionar(usuario);
@@ -77,14 +102,22 @@ public class Main {
     }
 
     public static void emprestarLivro(Scanner sc, EmprestimoService emprestimoService) {
-        System.out.print("\nId do Usuário: ");
-        int idUsuario = sc.nextInt();
-        System.out.print("\nId do Livro: ");
-        int idLivro = sc.nextInt();
-        System.out.print("\nTempo de Empréstimo (em dias): ");
-        int tempoEmDias = sc.nextInt();
+        int idUsuario = 0;
+        int idLivro = 0;
+        int tempoEmDias = 0;
+        try {
+            System.out.print("\nId do Usuário: ");
+            idUsuario = sc.nextInt();
+            System.out.print("\nId do Livro: ");
+            idLivro = sc.nextInt();
+            System.out.print("\nTempo de Empréstimo (em dias): ");
+            tempoEmDias = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Digite um número válido");
+            sc.nextLine();
+        }
 
         emprestimoService.emprestar(idUsuario, idLivro, tempoEmDias);
-
+        System.out.printf("Empréstimo realizado com sucesso! Prazo: %s", tempoEmDias);
     }
 }
